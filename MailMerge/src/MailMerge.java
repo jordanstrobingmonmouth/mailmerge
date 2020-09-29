bda4fd55cce02eb041e703c0ae25160726f68a94
jordanstrobing <jordanstrobing@localhost>
2020-09-29 13:05:39 -0400

    This is commit 2, used the replace tool

--- MailMerge/src/MailMerge.java
+++ MailMerge/src/MailMerge.java
@@ -4,18 +4,24 @@ public class MailMerge
 {
 	public static void main(String []args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Dear <first> <last>");
+		String firstlast = "Dear <first> <last>";
+		System.out.println(firstlast);
 		String first = in.next();
 		String last = in.next();
-		System.out.println("Please confirm your address is <house> <street>");
+		
+		String address = "Please confirm your address is <house> <street>";
+		System.out.println(address);
 		String housenum = in.next();
 		String street = in.next();
 		String streettype = in.next();
-		System.out.println("Your answer is <yes or no> ");
+		
+		String yesno = "Your answer is <yes or no> ";
+		System.out.println(yesno);
 		String yorn = in.next();
-		System.out.println("Dear "+ first + " " + last);
-		System.out.println("Please confirm that your address is " + housenum + " " + street + " " + streettype);
-		System.out.println("Your answer is: " + yorn);
+		
+		System.out.println(firstlast.replace("<first> <last>", first + " " + last));
+		System.out.println(address.replace("<house> <street>", housenum + " " + street + " " + streettype));
+		System.out.println(yesno.replace("<yes or no>", yorn));
 
 
 }
